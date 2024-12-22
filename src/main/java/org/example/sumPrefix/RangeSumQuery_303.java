a064af04d514014ca996800e1278e998a0cd1fc2
tania.sanjid <taniasanjid08@gmail.com>
2024-12-22 17:02:15 -0500

    implemeted both iterative and prefix sum method

--- src/main/java/org/example/sumPrefix/RangeSumQuery_303.java
+++ src/main/java/org/example/sumPrefix/RangeSumQuery_303.java
@@ -3,6 +3,7 @@ package org.example.sumPrefix;
 import java.util.Scanner;
 
 public class RangeSumQuery_303 {
+    /*Using iteratice, for smaller size of array
     static int[] nums;
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
@@ -30,4 +31,40 @@ public class RangeSumQuery_303 {
         }
         return sum;
     }
+}
+*/
+
+static int[] prefixSum;
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String[] s = sc.nextLine().split(" ");
+    int[] nums = new int[s.length];
+
+    for (int i = 0; i < s.length; i++) {
+        nums[i] = Integer.parseInt(s[i]);
+    }
+
+    // Precompute prefix sums
+    prefixSum = new int[nums.length];
+    prefixSum[0] = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+        prefixSum[i] = prefixSum[i - 1] + nums[i];
+    }
+
+    // Read range query
+    int left = sc.nextInt();
+    int right = sc.nextInt();
+
+    // Calculate and print the result
+    int result = sumRange(left, right);
+    System.out.println(result);
+}
+
+public static int sumRange(int left, int right) {
+    if (left == 0) {
+        return prefixSum[right];
+    } else {
+        return prefixSum[right] - prefixSum[left - 1];
+    }
+}
 }
